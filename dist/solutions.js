3d958b5d6ee0d715999a17f153614388b98cba6a
Utso Sarkar <utsosarkar7870@gmail.com>
2025-11-22 12:24:25 +0600

    type script upload first problem

---
+++ dist/solutions.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let course = "TypeScript Basics";
+course = 102;
+console.log(course);
+//# sourceMappingURL=solutions.js.map
